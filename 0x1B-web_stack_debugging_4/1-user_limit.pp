6e8e307787a7c5f0b320ff9c8c487cc52911eb02
Bright Adewole <54893652+Brheight@users.noreply.github.com>
2024-08-12 13:27:43 +0100

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,4 +0,0 @@
-{ 'change-os-configuration-for-holberton-user':
-  command => "/bin/echo 'holberton soft nofile 65536' >> /etc/security/limits.conf && /bin/echo 'holberton hard nofile 65536' >> /etc/security/limits.conf && /bin/echo 'session required pam_limits.so' >> /etc/pam.d/common-session",
-  unless  => "grep -q 'holberton' /etc/security/limits.conf",
-}
