a0872df70a6f86119fd8d430abed7aa9f96bd8f0
bright <brightoadewole@gmail.com>
2024-08-14 00:24:53 +0000

    dfd

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,30 @@
+#!/usr/bin/python3
 """
-subs
+Module to query the Reddit API for the number of subscribers
+for a given subreddit using PRAW.
 """
 
-import requests
+import praw
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, the function returns 0.
+    Queries the Reddit API using PRAW to return the number of subscribers
+    for a given subreddit. Returns 0 if the subreddit is invalid.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: Number of subscribers or 0 if the subreddit is invalid.
     """
-    
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        'User-Agent': 'MyRedditApp/1.0 (by /u/ctbrjg1067)',
-        'Accept': 'application/json'
-    }
+    reddit = praw.Reddit(
+        client_id='LSNpbK15FsQpfsf_a_bgTA',
+        client_secret='vu-CHv2VGwI1XhIvThrlbcrgNcXseg',
+        user_agent='alxse script by u/ctbrjg1067'
+    )
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data'].get('subscribers', 0)
-        elif response.status_code == 403:
-            print("Error: Access forbidden for subreddit '{}'.".format(subreddit))
-            return 0
-        elif response.status_code == 404:
-            print("Error: Subreddit '{}' not found.".format(subreddit))
-            return 0
-        else:
-            print("Error: Received status code {}".format(response.status_code))
-            return 0
-    except requests.RequestException as e:
-        print("RequestException: {}".format(e))
+        sub = reddit.subreddit(subreddit)
+        return sub.subscribers
+    except Exception:
         return 0
